eea88ee684500ee99c3d94406239bbc4fcd4192a
Karan Shah <karanshah0206@outlook.com>
2020-05-13 20:15:57 +0530

    Drawing And Tools Select Functionality JS

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,4 @@
-var context, contexto, canvas, canvaso, container, tool, tool_default='chalk';
+var context, contexto, canvas, canvaso, container, tool, tool_default='chalk', tool_select, func, tools={};
 
 function init () {
     // Getting Canvas Into canvaso Variable
@@ -34,4 +34,74 @@ function init () {
     context.lineWidth = 1.0;
     context.fillStyle = '#424242';
     context.fillRect(0, 0, 897, 532);
+
+    // Tool Selector
+    tool_select = document.getElementById('selector');
+    if (!tool_select) {
+        alert('Error: Cannot Access Tool Selector.');
+        return;
+    }
+    tool_select.addEventListener('change', ev_tool_change, false);
+    if (toools[tool_default]) {
+        tool = new tools[tool_default]();
+        tool_select.value = tool_default;
+    }
+    
+    // Canvas Listeners
+    canvas.addEventListener('mousedown', ev_canvas, false);
+    canvas.addEventListener('mousemove', ev_canvas, false);
+    canvas.addEventListener('mouseup', ev_canvas, false);
+
+    // Getting Mouse Position
+    function ev_canvas (ev) {
+        if (ev.layerX || ev.layerX == 0) {
+            ev._x = ev.layerX;
+            ev._y = ev.layerY;
+        }
+        else if (ev.offsetX || ev.offsetX == 0) {
+            ev._x = ev.offsetX;
+            ev._y = ev.offsetY;
+        }
+        func = tool[ev.type];
+        if (func) {
+            func(ev);
+        }
+    }
+
+    // Tool Selectro Value Changed
+    function ev_tool_change (ev) {
+        if (tools[this.value]) {
+            tool = new tools[this.value]();
+        }
+    }
+
+    // Overlap Temporary Canvas On Original
+    function img_update () {
+        contexto.drawImage(canvas, 0, 0);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    // Chalk Tool
+    tools.chalk = function () {
+        var tool = this;
+        this.started = false;
+        this.mousedown = function (ev) {
+            context.beginPath();
+            context.moveTo(ev._x, ev._y);
+            tool.started = true;
+        };
+        this.mousemove = function (ev) {
+            if (tool.started) {
+                context.lineTo(ev._x, ev._y);
+                context.stroke();
+            }
+        };
+        this.mouseup = function (ev) {
+            if (tool.started) {
+                tool.mousemove(ev);
+                tool.started = false;
+                img_update();
+            }
+        };
+    };
 }
